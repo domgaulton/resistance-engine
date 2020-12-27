b8d4c4cce2d0e943515bea1b18fd897beec5dd9b
Dom Gaulton <domgaulton@Doms-MacBook-Pro.local>
2020-12-27 13:00:19 +0000

    Update how many spies needed and game logic

---
+++ src/helpers/howManySpiesRequired.js
@@ -0,0 +1,31 @@
+export default gameObject => {
+  const players = gameObject.players && gameObject.players.length;
+  const round = gameObject.round;
+
+  let spies = 1;  
+  switch(players) {
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+      switch(round) {
+        case 0:
+        case 1:
+        case 2:
+          spies = 1;
+          break
+        case 3:
+          spies = 2;
+          break
+        case 4:
+          spies = 1;
+        default:
+          spies = 1;
+      }
+      break
+    default:
+      spies = 1;
+  }
+
+  return spies
+};
